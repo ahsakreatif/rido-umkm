e6b055922ec302ae036791560e4908aa5b61ca78
firmanhdt <firmanhidayat7007@gmail.com>
2024-10-17 08:10:34 +0700

    edit file regist

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'));
 Route::get('/products', fn () => view('products'));
-Route::get('/register',fn() => view('auth.register-user'));
+Route::get('/register',fn() => view('auth.register'));
